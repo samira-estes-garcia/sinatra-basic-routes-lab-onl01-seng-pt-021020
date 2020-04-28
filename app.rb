4a1ee545ec1f176b0a3e64b647b86e54d6997380
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-28 00:50:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Samira!"
+  end
+  
+  get '/hometown' do
+    "My hometown is San Antonio"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Shinee!"
+  end
+  
 end
